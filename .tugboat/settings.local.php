14da4aae18de291b6a574e5bfd33973483f2e318
Zequi Vazquez <zequi@lullabot.com>
2023-09-04 17:49:32 +0200

    Initial commit

---
+++ .tugboat/settings.local.php
@@ -0,0 +1,18 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'tugboat',
+  'username' => 'tugboat',
+  'password' => 'tugboat',
+  'prefix' => '',
+  'host' => 'mysql',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
+// Use the TUGBOAT_REPO_ID to generate a hash salt for Tugboat sites.
+$settings['hash_salt'] = hash('sha256', getenv('TUGBOAT_REPO_ID'));
+
+$settings['trusted_host_patterns'] = [
+  '^(.*)\.tugboatqa\.com$',
+];
